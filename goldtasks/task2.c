fb65a0d017bd3a572fa6efa4466aa7d1bb35db41
AdamRobinson9 <146648076+AdamRobinson9@users.noreply.github.com>
2023-10-17 15:33:45 +0000

    Final

--- goldtasks/task2.c
+++ goldtasks/task2.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int num = 0;
+    int choice = 0;
 
-    while (choice != -1)
-    printf("Make a choice\n");
-    scanf("%d", &choice);
+    while (choice != -1){
+        printf("Make a choice\n");
+        scanf("%d", &choice);
+        if (choice == -1){
+            printf("Termination value.");
+        }
+        else if (choice < 0 || choice > 100){
+            printf("Not in the range.\n");
+        }
+        else {
+            printf("Number in range.\n");
+        }
+    }
+    
 }
